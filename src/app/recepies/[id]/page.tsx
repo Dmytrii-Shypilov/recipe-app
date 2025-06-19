ef781d18f422bbdb707d8e7b58fef1008d70e3aa
Dmytrii Shypilov <dmytriishypilov@gmail.com>
2025-06-19 22:28:40 +0300

    removed dummy picture

--- src/app/recepies/[id]/page.tsx
+++ src/app/recepies/[id]/page.tsx
@@ -11,6 +11,12 @@ type RecipePageProps = {
 
 export const revalidate = 60;
 
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
+  }
+}
+
 const RecipePage = async ({ params }: RecipePageProps) => {
   const { id } = await params;
 
